2d22893e0688a98b4bb4590df847d78c29049d5e
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-10-18 21:13:00 +0300

    Manager.deviceList supported vendors filter

--- src/zebra/manager/manager.ts
+++ src/zebra/manager/manager.ts
@@ -2,6 +2,12 @@ import { EventEmitter } from 'events';
 import * as usb from 'usb';
 import * as usbDetection from 'usb-detection';
 
+const supportedVendors = [
+  0xA5F,  // Zebra
+  0xBDA,  // Test: Realtek
+  0x4F2,  // Test: Standart USB Host Controller
+];
+
 export class Manager extends EventEmitter {
   constructor() {
     super();
@@ -13,13 +19,16 @@ export class Manager extends EventEmitter {
     usbDetection.on('change', (device) => this.emit('change', device));
   }
 
+  /**
+   * List of attached usb devices filtered by supported vendors.
+   */
   public get deviceList(): Promise <Device[]> {
     return new Promise((resolve, reject) => {
       usbDetection.find((error, devices) => {
         if (error !== undefined) {
           reject();
         } else {
-          resolve(devices);
+          resolve(devices.filter((device) => supportedVendors.indexOf(device.vendorId) !== -1));
         }
       });
     });
