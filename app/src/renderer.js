1feea5de56685677db8a5a50cdb5452d70f1a1d0
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-10-18 22:51:07 +0300

    fix: .gitignore

--- app/src/renderer.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var m = require("mithril");
-var root = document.getElementById('app');
-m.render(root, m('div', 'hello from mithril'));
-//# sourceMappingURL=renderer.js.map
