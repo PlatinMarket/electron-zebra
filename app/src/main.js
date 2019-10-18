5c01b88fc90f7af7a69b05ec2bdb85af4cf7580a
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-10-18 20:09:38 +0300

    Folder change

---
+++ app/src/main.js
@@ -0,0 +1,21 @@
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var path = require("path");
+var mainWindow;
+function createWindow() {
+    mainWindow = new electron_1.BrowserWindow({
+        width: 1024,
+        height: 768,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.loadFile(path.join(__dirname, "../app.html"));
+    mainWindow.webContents.openDevTools({ mode: "detach" });
+    mainWindow.on("closed", function () {
+        mainWindow = null;
+    });
+}
+electron_1.app.on("ready", createWindow);
+//# sourceMappingURL=main.js.map
